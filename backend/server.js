66955c86c3c85641fedcee8ada15e657f084e9a3
Oliveszn <98136477+Oliveszn@users.noreply.github.com>
2024-10-11 22:07:26 +0100

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["https://bankist-plum.vercel.app/"],
+    origin: ["https://bankist-plum.vercel.app"],
     methods: ["POST", "GET"],
     credentials: true,
   })

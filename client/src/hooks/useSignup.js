1aedbe7f27ff0630631b3877c5bc409605592d8c
Oliveszn <98136477+Oliveszn@users.noreply.github.com>
2024-10-11 22:10:29 +0100

    Update useSignup.js

--- client/src/hooks/useSignup.js
+++ client/src/hooks/useSignup.js
@@ -10,7 +10,7 @@ export const useSignup = () => {
     setIsLoading(true);
     setError(null);
 
-    const response = await fetch("https://bankist-api.vercel.app/", {
+    const response = await fetch("https://bankist-api.vercel.app", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ username, password }),
